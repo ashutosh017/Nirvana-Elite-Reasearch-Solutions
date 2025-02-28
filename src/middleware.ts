3121b3dcf3558c94d6c4b61a42144cc57a5b0a37
ashutosh017 <wwwashutosh225@gmail.com>
2025-02-28 10:50:44 +0530

    debugging

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,22 +3,34 @@ import { NextRequest, NextResponse } from "next/server";
 import { verifyToken } from "./app/_utils/jwt";
 
 export async function middleware(req: NextRequest) {
-  console.log("middleware called");
-  const cooki = await cookies();
-  const token= cooki.get("token")?.value ;
+  try {
+    console.log("middleware called");
+    const cooki = await cookies();
+    const token = cooki.get("token")?.value;
 
-  if (!token) {
-    console.log("token not found");
-    return NextResponse.redirect(new URL("/admin/login", req.url));
-  }
-  console.log("token recieved in middleware: ", token);
+    if (!token) {
+      console.log("token not found");
+      return NextResponse.redirect(new URL("/admin/login", req.url));
+    }
+    console.log("token recieved in middleware: ", token);
 
-  const verify = verifyToken(token);
-  if (!verify) {
-    console.log("not verified");
-    return NextResponse.redirect(new URL("/admin/login", req.url));
+    const verify = verifyToken(token);
+    if (!verify) {
+      console.log("not verified");
+      return NextResponse.redirect(new URL("/admin/login", req.url));
+    }
+    console.log("Verify: ", verify);
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        msg: "some error occured",
+        error,
+      }),
+      {
+        status: 403,
+      }
+    );
   }
-  console.log("Verify: ", verify);
 }
 
 export const config = {
